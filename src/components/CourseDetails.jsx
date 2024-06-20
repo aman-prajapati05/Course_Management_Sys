d2bd3d5a06d0ea7453be438c43537c9b4c6ed9ca
Aman Prajapati <pjaman05671@gmail.com>
2024-06-20 16:06:18 +0530

    added icons

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { IoCheckmark } from "react-icons/io5";
+import { IoCheckmark, IoStarHalf } from "react-icons/io5";
 import { useParams } from "react-router-dom";
 import { fetchCourseById, onAuthStateChanged ,auth} from "../firebase/FirebaseConfig";
 import { useState, useEffect } from "react";
 import Accordion from "./Accordion";
 import { Link } from "react-router-dom";
 import { addCourseToCart } from "../firebase/FirebaseConfig";
+import { IoStar } from "react-icons/io5";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { CiGlobe } from "react-icons/ci";
+import { MdSubtitles } from "react-icons/md";
 
 const CourseDetails = () => {
   const {courseId} = useParams();
@@ -75,14 +79,15 @@ const CourseDetails = () => {
       <div className="hidden lg:block bg-black  h-80">
         <div className="mx-auto flex flex-wrap max-w-7xl justify-between px-4 py-8 sm:px-6 lg:px-8">
           <div className="text-white lg:w-[60%]">
-            <h1 className="text-xs font-bold">{course.category}</h1>
+            <h1 className="text-xs font-bold">{course.courseCategory}</h1>
             <p className="text-3xl font-bold pt-3">{course.title}</p>
             <p className="text-sm pt-3">{course.one_description}</p>
             <p className="pt-4">{course.instructor}</p>
+            <p className="my-3 flex gap-1 font-normal  dark:text-gray-400"><IoStar/><IoStar/><IoStar/><IoStar/><IoStarHalf /></p>
             <div className="flex justify-start pt-4 gap-4">
-              <div>Last updated 12/2022</div>
-              <div>English</div>
-              <div>English [Auto]</div>
+              <div className="flex gap-1 items-center"><IoMdInformationCircleOutline />Last updated 12/2022</div>
+              <div className="flex gap-1 items-center"><CiGlobe />English</div>
+              <div className="flex gap-1 items-center"><MdSubtitles />English [Auto]</div>
             </div>
           </div>
 
@@ -91,7 +96,7 @@ const CourseDetails = () => {
               <img className="w-full" src={course.image} alt="" />
             </div>
             <div className="flex flex-col mx-8 gap-4">
-            <div className="text-4xl font-bold text-purple-500 py-2">Price:  $544</div>
+            <div className="text-4xl font-bold text-purple-500 py-2">Price:  FREE</div>
             <button onClick={handleAddToCart} className="  text-white bg-black text-lg font-bold p-4 px-12 ">
             <Link to='/AddtoCart' className="">Enroll Now</Link>
         </button>
@@ -99,7 +104,7 @@ const CourseDetails = () => {
           (  <button  className="  text-black border  text-lg font-bold p-4 px-11 ">
           <Link to ='/AddtoCart'>Go to Cart</Link>
         </button>):
-        (<button onClick={handleAddToCart} className="  text-black border  text-lg font-bold p-4 px-11 ">
+        (<button onClick={handleAddToCart} className="  text-black border mb-8 text-lg font-bold p-4 px-11 ">
           Add to Cart
         </button>)}
             </div>
@@ -108,7 +113,7 @@ const CourseDetails = () => {
       </div>
       <div className="lg:hidden flex flex-col items-center w-[90%] gap-4 mx-auto">
         <div className="text-xs font-bold ">
-          {course.category}
+          {course.courseCategory}
         </div>
         <div className="w-[90%] md:w-[70%]">
           <img className="w-full" src={course.image} alt="" />
@@ -138,38 +143,24 @@ const CourseDetails = () => {
         <div className=" border-2 border-black lg:w-[50%]">
           <p className="text-3xl font-bold py-2 px-5">What You'll learn</p>
           <div className="grid grid-cols-2 px-5 py-4 pb-7">
-            <div className="flex ">
-              <div className="m-1">
-                <IoCheckmark />
+          {course.Outcome.map((outcome, index) => (
+              <div className="flex" key={index}>
+                <div className="m-1">
+                  <IoCheckmark />
+                </div>
+                <p className="px-1">{outcome}</p>
               </div>
-              <p className="px-1">Creating user accounts</p>
-            </div>
-            <div className="flex ">
-              <div className="m-1">
-                <IoCheckmark />
-              </div>
-              <p className="px-1">Creating user accounts</p>
-            </div>
-            <div className="flex ">
-              <div className="m-1">
-                <IoCheckmark />
-              </div>
-              <p className="px-1">Creating user accounts</p>
-            </div>
-            <div className="flex ">
-              <div className="m-1">
-                <IoCheckmark />
-              </div>
-              <p className="px-1">Creating user accounts</p>
-            </div>
+            ))}
           </div>
         </div>
       </div>
       <div className="mx-auto flex flex-wrap max-w-7xl justify-between px-4 py-8 sm:px-6 lg:px-8">
         <div className=" lg:w-[50%]">
           <p className="text-3xl font-bold py-2 px-5">Course Contents</p>
+          <p className="px-5">Course Duration:{course.duration}</p>
           {/* Accordion */}
-          <Accordion/>
+          <div className="px-5"><Accordion contents={course.content} /></div>
+          
         </div>
       </div>
 
