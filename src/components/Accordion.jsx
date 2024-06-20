28306271e066c7d96c4da7c103e0ee4e7afda058
Aman Prajapati <pjaman05671@gmail.com>
2024-06-20 15:59:49 +0530

    FIXED ACCORDION

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Accordion = () => {
+const Accordion = ({ contents }) => {
   const [openIndex, setOpenIndex] = useState(null);
 
   const handleToggle = (index) => {
@@ -9,77 +9,16 @@ const Accordion = () => {
 
   return (
     <div id="accordion-collapse" data-accordion="collapse">
-      <AccordionItem
-        index={1}
-        openIndex={openIndex}
-        handleToggle={handleToggle}
-        title="What is Flowbite?"
-        content={
-          <>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
-            </p>
-            <p className="text-gray-500 dark:text-gray-400">
-              Check out this guide to learn how to{" "}
-              <a href="/docs/getting-started/introduction/" className="text-blue-600 dark:text-blue-500 hover:underline">
-                get started
-              </a>{" "}
-              and start developing websites even faster with components on top of Tailwind CSS.
-            </p>
-          </>
-        }
-      />
-      <AccordionItem
-        index={2}
-        openIndex={openIndex}
-        handleToggle={handleToggle}
-        title="Is there a Figma file available?"
-        content={
-          <>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.
-            </p>
-            <p className="text-gray-500 dark:text-gray-400">
-              Check out the{" "}
-              <a href="https://flowbite.com/figma/" className="text-blue-600 dark:text-blue-500 hover:underline">
-                Figma design system
-              </a>{" "}
-              based on the utility classes from Tailwind CSS and components from Flowbite.
-            </p>
-          </>
-        }
-      />
-      <AccordionItem
-        index={3}
-        openIndex={openIndex}
-        handleToggle={handleToggle}
-        title="What are the differences between Flowbite and Tailwind UI?"
-        content={
-          <>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.
-            </p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.
-            </p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Learn more about these technologies:
-            </p>
-            <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
-              <li>
-                <a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">
-                  Flowbite Pro
-                </a>
-              </li>
-              <li>
-                <a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 dark:text-blue-500 hover:underline">
-                  Tailwind UI
-                </a>
-              </li>
-            </ul>
-          </>
-        }
-      />
+      {Object.keys(contents).map((key, index) => (
+        <AccordionItem
+          key={index}
+          index={index}
+          openIndex={openIndex}
+          handleToggle={handleToggle}
+          title={key}
+          content={contents[key]}
+        />
+      ))}
     </div>
   );
 };
@@ -92,14 +31,14 @@ const AccordionItem = ({ index, openIndex, handleToggle, title, content }) => {
       <h2 id={`accordion-collapse-heading-${index}`}>
         <button
           type="button"
-          className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200  focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+          className="flex items-center justify-between w-full p-5 font-medium text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
           onClick={() => handleToggle(index)}
           aria-expanded={isOpen}
           aria-controls={`accordion-collapse-body-${index}`}
         >
           <span>{title}</span>
           <svg
-            className={`w-3 h-3 ${isOpen ? 'rotate-180' : ''} shrink-0`}
+            className={`w-3 h-3 ${isOpen ? 'rotate-180' : ''}`}
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -120,7 +59,7 @@ const AccordionItem = ({ index, openIndex, handleToggle, title, content }) => {
         className={`${isOpen ? 'block' : 'hidden'}`}
         aria-labelledby={`accordion-collapse-heading-${index}`}
       >
-        <div className="p-5 border  border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+        <div className="p-5 border border-gray-200 dark:border-gray-700 dark:bg-gray-900">
           {content}
         </div>
       </div>
@@ -129,3 +68,4 @@ const AccordionItem = ({ index, openIndex, handleToggle, title, content }) => {
 };
 
 export default Accordion;
+
