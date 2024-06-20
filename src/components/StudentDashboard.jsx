c7233f5d11cc9b9309a78ceb5829180d2b799fd8
Aman Prajapati <pjaman05671@gmail.com>
2024-06-20 12:13:47 +0530

    added functionality

--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -1,22 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import Card from './Card'
+import Subs from './Subs'
+import Unsubs from './Unsubs'
+import { getCourseDetails, getCurrentUser, getUserCourses, markCourseAsComplete } from '../firebase/FirebaseConfig'
 
 const StudentDashboard = () => {
+
+  const [user, setUser] = useState(null);
+  const [coursesData, setCoursesData] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const currentUser = await getCurrentUser();
+        setUser(currentUser);
+      } catch (error) {
+        console.error('Error fetching current user:', error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      const fetchUserCourses = async () => {
+        try {
+          getUserCourses(user.uid, async (courseIds) => {
+            if (courseIds.length > 0) {
+              const coursesData = await getCourseDetails(courseIds);
+              setCoursesData(coursesData);
+            } else {
+              setCoursesData([]);
+            }
+          });
+        } catch (error) {
+          console.error('Error fetching user courses:', error);
+        }
+      };
+
+      fetchUserCourses();
+    }
+  }, [user]);
+
+  const handleMarkAsComplete = async (courseId) => {
+    try {
+      await markCourseAsComplete(user.uid, courseId);
+      setCoursesData(coursesData.map(course => 
+        course.id === courseId ? { ...course, completed: true } : course
+      ));
+    } catch (error) {
+      console.error('Error marking course as complete:', error);
+    }
+  };
+
+  const completedCourses = coursesData.filter(course => course.completed);
+  const incompleteCourses = coursesData.filter(course => !course.completed);
+
+
+  
   return (
     <>
     <Navbar/>
     
         
-            {/* <div className='h-40  bg-black'>
-                <div className='  mx-auto flex flex-wrap max-w-7xl justify-between px-4 py-8 sm:px-6 lg:px-8'>
-                    <p className='text-white text-6xl'>My Courses</p>
+            <div className='md:h-40 h-28  bg-black'>
+                <div className='  mx-auto flex flex-wrap max-w-7xl items-baseline justify-between px-4 py-8 sm:px-6 lg:px-8'>
+                    <p className='text-white text-5xl md:text-6xl pt-4 md:pt-0'>My Courses</p>
                     
                 </div>
             </div>
-            <div className=' mx-auto flex flex-wrap max-w-7xl justify-between px-4 py-8 sm:px-6 lg:px-8'>
-                        <Card/>
-            </div> */}
+            <Unsubs courses={incompleteCourses} onMarkAsComplete={handleMarkAsComplete} />
+            <Subs courses={completedCourses} />
 
         
  
