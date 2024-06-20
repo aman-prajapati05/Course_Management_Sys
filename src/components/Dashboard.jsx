9373b20bbae3203fc38d29338c462cc3145579cd
Aman Prajapati <pjaman05671@gmail.com>
2024-06-20 12:12:56 +0530

    added functionality

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Card from './Card'
 import Hero from './Hero'
 import { FooterThree } from './Footer'
+import { useState,useEffect } from 'react'
+import { fetchCourses } from '../firebase/FirebaseConfig';
+
 
 const Dashboard = () => {
+  const [courses, setCourses] = useState([]);
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCoursesData = async () => {
+      try {
+        const coursesData = await fetchCourses();
+        setCourses(coursesData);
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoursesData();
+  }, []);
+
+  const handleSearch = (searchTerm) => {
+    const filtered = courses.filter((course) =>
+      course.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredCourses(filtered);
+    console.log('Filtered:', filtered);
+  };
+
+
+
   return (
    <>
-   <Navbar/>
-   <Hero/>
+   <Navbar onSearch = {handleSearch}/>
+   <Hero courses={filteredCourses.length > 0 ? filteredCourses : courses} loading={loading} />
    <FooterThree/>
+   
    </>
   )
 }
