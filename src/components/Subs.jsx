8ad17ddd54f82e3d2f88bae838f41e5a50319a20
Aman Prajapati <pjaman05671@gmail.com>
2024-06-20 16:03:07 +0530

    fixed

--- src/components/Subs.jsx
+++ src/components/Subs.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Card from './Card'
 
-const Subs = () => {
+const Subs = ({courses}) => {
   return (
     <>
         <div className='mx-auto  max-w-7xl border-b-2 px-4 py-8 sm:px-6 text-black lg:px-8'>
             <div className='text-3xl font-bold text-purple-500'>Completed </div>
         </div>
             <div className=' mx-auto flex flex-wrap max-w-7xl justify-between px-4 py-8 sm:px-6 lg:px-8'>
-             <Card/>
+            <div className='grid grid-cols-4'>
+      {courses.map(course => (
+        <div key={course.id} className='w-full p-4'>
+          <div className='bg-white p-6 rounded-lg shadow-lg ' >
+            <img className='h-40 rounded w-full object-cover object-center mb-6' src={course.image} alt={course.title} />
+            <h3 className='tracking-widest text-indigo-500 text-xs font-medium title-font'>{course.instructor}</h3>
+            <h2 className='text-lg text-gray-900 font-medium title-font mb-4'>{course.title}</h2>
+            <p className='leading-relaxed text-base'>{course.one_description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
             </div>
     </>
   )
--- src/components/Unsubs.jsx
+++ src/components/Unsubs.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import Card from './Card'
 
-const Unsubs = () => {
+const Unsubs = ({ courses, onMarkAsComplete }) => {
   return (
     <>
         <div className='mx-auto  max-w-7xl border-b-2 px-4 py-8 sm:px-6 text-black lg:px-8'>
             <div className='text-3xl font-bold text-purple-500'>Uncompleted </div>
         </div>
             <div className=' mx-auto flex flex-wrap max-w-7xl justify-between px-4 py-8 sm:px-6 lg:px-8'>
-             <Card/>
+            <div className='grid grid-cols-4'>
+      {courses.map(course => (
+        <div key={course.id} className='w-full  p-4'>
+          <div className='bg-white p-6 rounded-lg shadow-lg'>
+            <img className='h-40 rounded w-full object-cover object-center mb-6' src={course.image} alt={course.title} />
+            <h3 className='tracking-widest text-indigo-500 text-xs font-medium title-font'>{course.instructor}</h3>
+            <h2 className='text-lg text-gray-900 font-medium title-font mb-4'>{course.title}</h2>
+            <p className='leading-relaxed text-base'>{course.one_description}</p>
+            <button onClick={() => onMarkAsComplete(course.id)} className='bg-indigo-500 text-white px-4 py-2 rounded'>
+              Mark as Complete
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
             </div>
     </>
   )
